51ada097596d79a223a0226831a74a4b4c9d1727
Thai Hoang Bao <thaihoangbao@outlook.com>
2024-12-09 21:04:36 +0700

    Update

--- src/ss3_java_overview/AgeCal.java
+++ src/ss3_java_overview/AgeCal.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class AgeCal {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap vao nam sinh cua ban: ");
+
         int yearBirth = sc.nextInt();
         int yearNow = java.time.LocalDate.now().getYear();
+        
         System.out.println("Ban da " + (yearNow - yearBirth) + " tuoi roi");
     }
 }
--- src/ss3_java_overview/CalAB.java
+++ src/ss3_java_overview/CalAB.java
@@ -6,10 +6,13 @@ public class CalAB {
     public static void main(String[] args) {
         int a,b;
         Scanner sc = new Scanner(System.in);
+        //Nhập a
         System.out.print("Nhap vao so a : ");
         a = sc.nextInt();
+        //Nhập b
         System.out.print("Nhap vao so b : ");
         b = sc.nextInt();
+        //In ra màn hình
         System.out.println("Tong cua " + a + " va " + b + " la: " + (a+b));
         System.out.println("Hieu cua " + a + " va " + b + " la: " + (a-b));
         System.out.println("Tich cua " + a + " va " + b + " la: " + (a*b));
